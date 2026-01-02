6513b435c145008a308ad89dab0dbbbfe229a340
Umamaheswari-gude <umamaheswari.g@everest.engineering>
2026-01-02 18:57:21 +0530

    Add validation to the fields

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -2,6 +2,12 @@ import { Request, Response } from "express";
 import { addTasks } from "../services/todoService";
 
 export const addTask = async (req: Request, res: Response) => {
+  const { name, description, status, priority, deadline } = req.body;
+  if (!name || !description || !status || !priority || !deadline) {
+    res.status(400).json({
+      message: "All fields are required",
+    });
+  }
   try {
     const task = await addTasks(req.body);
     res.status(201).json(task);
